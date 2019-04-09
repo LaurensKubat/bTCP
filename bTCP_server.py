37d4bac90d18161156c91b5b79e607e8e6f41ea3
Laurens <laurens.kubat@gmail.com>
2019-04-09 13:19:16 +0200

    WIP on handling of multiple connections

--- bTCP_server.py
+++ bTCP_server.py
@@ -18,7 +18,7 @@ header_format = "I"
 # Server is the server
 class Server:
 
-    def __init__(self, port, ip, window, timeout, output):
+    def __init__(self, port, ip, window, timeout, output, cons:dict):
         self.port = port
         self.ip = ip
         self.window = window
@@ -27,6 +27,9 @@ class Server:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
         self.sock = sock
         self.sock.bind((server_ip, server_port))
+        # cons is a dict containing dicts for each connection. The second level dictionary contains
+        # WINDOW SIZE amount of packages
+        self.cons = cons
 
     def listen(self):
         while True:
@@ -35,18 +38,29 @@ class Server:
             packet.unpack(data)
             self.handle(packet)
 
+    # handle the packet based on the flags of the header packet
+    # TODO add handling of FIN, ADD a conn to the cons dict if msg is SYN. Add to correct conn dict
     def handle(self, packet: bTCP.packet.Packet):
         ok = packet.validate()
         if not ok:
-            self.sendNACK()
+            self.sendNACK(packet)
         elif packet.is_syn() and packet.is_ack():
-            self.sendACK()
+            self.sendACK(packet)
         elif packet.is_syn() and not packet.is_ack():
             self.sendSYNACK(packet)
+            self.cons[packet.header.stream_id] = {packet.header.SYN_number : packet}
         elif packet.is_ack():
             self.handleACK(packet)
+        else:
+            # if the connection isn't established yet, ignore the packet
+            if packet.header.stream_id not in self.cons:
+                return
+            # add the packet to the correct window
+            if packet.header.SYN_number not in self.cons[packet.header.stream_id]:
+                self.cons[packet.header.stream_id].update({packet.header.SYN_number : packet})
 
     def handleACK(self, packet: bTCP.packet.Packet):
+
 
     def sendSYNACK(self, packet: bTCP.packet.Packet):
 
