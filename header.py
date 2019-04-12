66c337dd8c0fe2b63152a9eef765eba9d071da7b
Laurens <laurens.kubat@gmail.com>
2019-04-12 21:05:48 +0200

    changed header so it uses structs to pack like a normal person

--- header.py
+++ header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import binascii
+import struct
 
 
 # Header represents the header of a bTCP package
 class Header:
 
     # TODO make sure the size of all the values are correct
-    def __init__(self, stream_id=bytes(4), syn_number=bytes(2), ack_number=bytes(2), flags=bytes(1), windows=bytes(1),
-                 data_length=(bytes(2)), checksum=bytes(4)):
+    def __init__(self, stream_id=0, syn_number=0, ack_number=0, flags=0, windows=0,
+                 data_length=0, checksum=0):
         self.stream_id = stream_id
         self.SYN_number = syn_number
         self.ACK_number = ack_number
@@ -26,55 +27,68 @@ class Header:
     # the example header in the given framework
     def serialize(self) -> bytes:
         header = bytearray()
-        header.append(int(self.stream_id))
-        header.append(int(self.SYN_number))
-        header.append(int(self.ACK_number))
-        header.append(int(self.flags))
-        header.append(int(self.windows))
-        header.append(int(self.data_length))
-        header.append(int.from_bytes(self.checksum, byteorder='big'))
+        b_stream_id = struct.pack("I", self.stream_id)
+        b_syn_number = struct.pack("H", self.SYN_number)
+        b_ack_number = struct.pack("H", self.ACK_number)
+        b_flags = struct.pack("c", self.flags)
+        b_windows = struct.pack("c", self.windows)
+        b_data_length = struct.pack("H", self.data_length)
+        b_checksum = struct.pack("I", self.checksum)
+        header = header.append(b_stream_id)
+        header = header.append(b_syn_number)
+        header = header.append(b_ack_number)
+        header = header.append(b_flags)
+        header = header.append(b_windows)
+        header = header.append(b_data_length)
+        header = header.append(b_checksum)
         return bytes(header)
 
     # deserialize bytes into the header object
     def deserialize(self, header: bytes):
-        self.stream_id = header[0:3]
-        self.SYN_number = header[4:6]
-        self.ACK_number = header[6:8]
-        self.flags = int(header[8])
-        self.windows = header[9]
-        self.data_length = header[10:12]
-        self.checksum = header[12:16]
+        self.stream_id = struct.unpack("I", header[0:3])
+        self.SYN_number = struct.unpack("H", header[4:6])
+        self.ACK_number = struct.unpack("H", header[6:8])
+        self.flags = header[8] # since one byte from bytes can be seen as an int, we do not unpack this
+        self.windows = header[9]  # same as above
+        self.data_length = struct.unpack("H", header[10:12])
+        self.checksum = struct.unpack("I", header[12:16])
 
     # returns true or false depending on if the header is correct
     def validate(self) -> bool:
-        checksumless = bytearray()
-        checksumless.append(self.stream_id)
-        checksumless.append(self.SYN_number)
-        checksumless.append(self.ACK_number)
-        checksumless.append(self.flags)
-        checksumless.append(self.windows)
-        checksumless.append(self.data_length)
-        checksum = binascii.crc32(checksumless)
+        header = bytearray()
+        b_stream_id = struct.pack("I", self.stream_id)
+        b_syn_number = struct.pack("H", self.SYN_number)
+        b_ack_number = struct.pack("H", self.ACK_number)
+        b_flags = struct.pack("c", self.flags)
+        b_windows = struct.pack("c", self.windows)
+        b_data_length = struct.pack("H", self.data_length)
+        header = header.append(b_stream_id)
+        header = header.append(b_syn_number)
+        header = header.append(b_ack_number)
+        header = header.append(b_flags)
+        header = header.append(b_windows)
+        header = header.append(b_data_length)
+        checksum = binascii.crc32(header)
         return checksum == self.checksum
 
     # is_syn checks if the SYN flag is on by bitshifting all the other bits away (done for efficiency)
     # the first bit is SYN, second is ACK third is FIN
     def is_syn(self) -> bool:
-        flagbuf = self.flags
-        synflag = flagbuf << 7 & 255
-        return bool(synflag)
+        flag_buf = self.flags
+        syn_flag = flag_buf << 7 & 255
+        return bool(syn_flag)
 
     def is_ack(self) -> bool:
-        flagbuf = self.flags
-        flagbuf = flagbuf >> 1
-        ackflag = flagbuf << 7 & 255
-        return bool(ackflag)
+        flag_buf = self.flags
+        flag_buf = flag_buf >> 1
+        ack_flag = flag_buf << 7 & 255
+        return bool(ack_flag)
 
     def is_fin(self) -> bool:
-        flagbuf = self.flags
-        flagbuf = flagbuf >> 2
-        finflag = flagbuf << 7 & 255
-        return bool(finflag)
+        flag_buf = self.flags
+        flag_buf = flag_buf >> 2
+        fin_flag = flag_buf << 7 & 255
+        return bool(fin_flag)
 
 
 def new_header(header: bytes) -> Header:
