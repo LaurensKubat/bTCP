178db8c8905e1f0eb006bcc072198d0aca1978eb
Laurens <laurens.kubat@gmail.com>
2019-04-13 10:06:15 +0200

    added unittests for header and fixed mistakes discovered by these tests

--- header.py
+++ header.py
@@ -19,55 +19,32 @@ class Header:
         self.checksum = checksum
 
     # genchecksum generates the checksum of the Header object
-    def genchecksum(self, header: bytes):
+    def genchecksum(self):
+        header = struct.pack("IHHBBH", self.stream_id, self.SYN_number, self.ACK_number, self.flags,
+                             self.windows, self.data_length, )
         self.checksum = binascii.crc32(header)
 
-    # TODO improve serialization such that the header is processed correctly
     # serialize the Header object into a bytearray, bytes are returned to ensure that it works the same as
     # the example header in the given framework
     def serialize(self) -> bytes:
-        header = bytearray()
-        b_stream_id = struct.pack("I", self.stream_id)
-        b_syn_number = struct.pack("H", self.SYN_number)
-        b_ack_number = struct.pack("H", self.ACK_number)
-        b_flags = struct.pack("c", self.flags)
-        b_windows = struct.pack("c", self.windows)
-        b_data_length = struct.pack("H", self.data_length)
-        b_checksum = struct.pack("I", self.checksum)
-        header = header.append(b_stream_id)
-        header = header.append(b_syn_number)
-        header = header.append(b_ack_number)
-        header = header.append(b_flags)
-        header = header.append(b_windows)
-        header = header.append(b_data_length)
-        header = header.append(b_checksum)
+        header = struct.pack("IHHBBHI", self.stream_id, self.SYN_number, self.ACK_number, self.flags,
+                             self.windows, self.data_length, self.checksum)
         return bytes(header)
 
     # deserialize bytes into the header object
     def deserialize(self, header: bytes):
-        self.stream_id = struct.unpack("I", header[0:3])
-        self.SYN_number = struct.unpack("H", header[4:6])
-        self.ACK_number = struct.unpack("H", header[6:8])
+        self.stream_id = struct.unpack("I", header[0:4])[0]
+        self.SYN_number = struct.unpack("H", header[4:6])[0]
+        self.ACK_number = struct.unpack("H", header[6:8])[0]
         self.flags = header[8] # since one byte from bytes can be seen as an int, we do not unpack this
         self.windows = header[9]  # same as above
-        self.data_length = struct.unpack("H", header[10:12])
-        self.checksum = struct.unpack("I", header[12:16])
+        self.data_length = struct.unpack("H", header[10:12])[0]
+        self.checksum = struct.unpack("I", header[12:16])[0]
 
     # returns true or false depending on if the header is correct
     def validate(self) -> bool:
-        header = bytearray()
-        b_stream_id = struct.pack("I", self.stream_id)
-        b_syn_number = struct.pack("H", self.SYN_number)
-        b_ack_number = struct.pack("H", self.ACK_number)
-        b_flags = struct.pack("c", self.flags)
-        b_windows = struct.pack("c", self.windows)
-        b_data_length = struct.pack("H", self.data_length)
-        header = header.append(b_stream_id)
-        header = header.append(b_syn_number)
-        header = header.append(b_ack_number)
-        header = header.append(b_flags)
-        header = header.append(b_windows)
-        header = header.append(b_data_length)
+        header = struct.pack("IHHBBH", self.stream_id, self.SYN_number, self.ACK_number, self.flags,
+                             self.windows, self.data_length,)
         checksum = binascii.crc32(header)
         return checksum == self.checksum
 
--- header_test.py
+++ header_test.py
@@ -14,13 +14,30 @@ class HeaderTestCase(unittest.TestCase):
         self.header = Header(1, 1, 1, 0, 0, 0)
 
     def test_checksum(self):
-        checksum = self.header.genchecksum(self.header.serialize())
+        checksum = 917831061
         self.assertEqual(checksum, binascii.crc32(self.header.serialize()), "header checksum is incorrect.")
 
     def test_serialize(self):
         serialized = self.header.serialize()
-        correct = bytes(b'\x01\x01\x01\x00\x00\x00\x00')
+        correct = bytes(b'\x01\x00\x00\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00')
         self.assertEqual(serialized, correct, "header is not serialized correct.")
+
+    def test_deserialize(self):
+        ser = bytes(b'\x01\x00\x00\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+        h = Header()
+        h.deserialize(ser)
+        self.assertEqual(h.stream_id, self.header.stream_id)
+        self.assertEqual(h.checksum, self.header.checksum)
+        self.assertEqual(h.data_length, self.header.data_length)
+        self.assertEqual(h.flags, self.header.flags)
+        self.assertEqual(h.ACK_number, self.header.ACK_number)
+        self.assertEqual(h.SYN_number, self.header.SYN_number)
+        self.assertEqual(h.windows, self.header.windows)
+
+    def test_validate(self):
+        self.assertFalse(self.header.validate())
+        self.header.genchecksum()  # helper function
+        self.assertTrue(self.header.validate())
 
 
 if __name__ == '__main__':
