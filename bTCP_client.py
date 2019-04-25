ec18b5448fc9dd42a46201530e01e677f119b92e
Laurens <laurens.kubat@gmail.com>
2019-04-25 13:33:05 +0200

    started on client.py

--- bTCP_client.py
+++ bTCP_client.py
@@ -3,6 +3,7 @@
 import socket, argparse
 from struct import *
 from bTCP import bTCPbase
+from bTCP.header import *
 
 # Handle arguments
 parser = argparse.ArgumentParser()
@@ -30,8 +31,29 @@ sock.sendto(udp_payload, (destination_ip, destination_port))
 # TODO add fragmenting of a file, keeping track of the window and sending messages according to the window.
 class Client(object):
 
-    def __init__(self, port, ip, window, timeout, output, cons: dict, sent: dict):
+    def __init__(self, port, ip, window, timeout, output, stream_id, cons: dict, sent: dict):
         self.base = bTCPbase.BasebTCP(port, ip, window, timeout, output, cons, sent)
         self.cur_syn = 0
         self.min_window = 0
         self.max_window = 0
+        self.stream_id = stream_id
+
+    # get_header in the client package should never handle flags, this is done in the bTCPbase
+    # or in the function getting the header
+    # the client does not have any windows in it's headers
+    def get_header(self, data_length: int) -> Header:
+        h = Header()
+        h.SYN_number = self.cur_syn
+        h.stream_id = self.stream_id
+        h.data_length = data_length
+        h.genchecksum()
+
+        return h
+
+    def openfile(self, filename):
+        f = open(filename, "rb")
+        try:
+            data = f.read(1000)
+            packet =
+        finally:
+            f.close()
