b7c2686b98df5c1387f16d53543bdc6fcec9fc34
Laurens <laurens.kubat@gmail.com>
2019-05-15 20:54:14 +0200

    WIP on the client

--- bTCP_client.py
+++ bTCP_client.py
@@ -2,7 +2,7 @@
 
 import socket, argparse
 from struct import *
-from bTCP.bTCPbase import *
+from bTCP.bTCPbaseRefactor import *
 
 # Handle arguments
 parser = argparse.ArgumentParser()
@@ -26,7 +26,47 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # send payload
 sock.sendto(udp_payload, (destination_ip, destination_port))
 
-client = BasebTCP(port=destination_port, ip=destination_ip, timeout=args[1],
-                  output= ,cons= ,sent = , window_size=args[0])
-client.
 
+class Client(object):
+
+    def __init__(self, filename: str, base: BasebTCP):
+        self.base = base
+        self.filename = filename
+        self.tosend = {}
+
+    def send_file(self):
+        self.create_packets()
+
+    def send_syn(self):
+        syn_pkt = Packet(b"")
+
+
+    def create_packets(self):
+        f = open(self.filename, "rb")
+        byte = f.read(1)
+        data_length = 0
+        pkt_num = 1
+        data = b""
+        while byte != b"":
+            data = data + byte
+            data_length = data_length + 1
+            # if we have read byte one thousand, we create a packet
+            if data_length == 1000:
+                pkt = Packet(data)
+                pkt.header.data_length = data_length
+                # we save the created packets without header in the format: Packet_number (packet
+                # 1 was created first); Packet, is sent, is acked.
+                self.tosend.update({pkt_num, (pkt, False, False)})
+                pkt_num = pkt_num + 1
+                data_length = 0
+                data = b""
+            byte = f.read(1)
+        # if we are done reading, but still have less than 1000 bytes left over, we create
+        # another packet
+        if data_length > 0:
+            pkt = Packet(data)
+            pkt.header.data_length = data_length
+            # we save the created packets without header in the format: Packet_number (packet
+            # 1 was created first); Packet, is sent, is acked.
+            self.tosend.update({pkt_num, (pkt, False, False)})
+        f.close()
